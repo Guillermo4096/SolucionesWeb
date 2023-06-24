cbc5348a4eb05c2a4248dfe705c00cd9ee6a045c
Jose <jose.gonzales.2084@gmail.com>
2023-06-23 22:56:38 -0500

    Correcciones previas a la T4

--- src/main/java/com/fijsac/proyecto/controller/model/service/IVentaService.java
+++ src/main/java/com/fijsac/proyecto/controller/model/service/IVentaService.java
@@ -7,6 +7,7 @@ import com.fijsac.proyecto.controller.model.entidad.Venta;
 public interface IVentaService {
     public List<Venta> mostrarVenta();
     public void guardarVenta(Venta venta);
+    public void editarVenta(Venta venta);
     public void eliminarVenta(Long id);
     public Venta buscarVenta(Long id);
 }
