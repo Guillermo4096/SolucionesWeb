7ba828bef0f93b87c872fe341bbda42dc1f115da
Jose <jose.gonzales.2084@gmail.com>
2023-06-29 23:00:06 -0500

    Funcionalidad stock de productos implementada

--- src/main/java/com/fijsac/proyecto/controller/model/service/IVentaService.java
+++ src/main/java/com/fijsac/proyecto/controller/model/service/IVentaService.java
@@ -6,7 +6,7 @@ import com.fijsac.proyecto.controller.model.entidad.Venta;
 
 public interface IVentaService {
     public List<Venta> mostrarVenta();
-    public void guardarVenta(Venta venta);
+    public String guardarVenta(Venta venta);
     public void editarVenta(Venta venta);
     public void eliminarVenta(Long id, Long cod_us);
     public Venta buscarVenta(Long id);
